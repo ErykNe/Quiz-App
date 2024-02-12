b0928e5fc067c750bb88527b180a0df9ab4bb4c8
Eryk <eryk.nejfert@wp.pl>
2024-02-12 23:02:31 +0100

    changes

--- src/home.page/homepage.component.ts
+++ src/home.page/homepage.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-homepage',
   standalone: true,
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css', './css/styles.css']
+  styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
 
